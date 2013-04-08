94a41303bfd48e1e6baa4fa77cb46fa5bc50010e
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-04-08 08:07:19 -0500

    Rearrange to if __main__.

--- ex_13_7_class.py
+++ ex_13_7_class.py
@@ -7,26 +7,6 @@ from scipy import optimize
 from scipy.linalg import inv
 # qed.econ.queensu.ca/jae/2003-v18.4/riphahn-wambach-million/rwm-data.zip
 # qed.econ.queensu.ca/jae/2003-v18.4/riphahn-wambach-million/readme.rwm.txt
-fldr = '/Users/tom/Dropbox/Economics/Econometrics_2/greene_examples/'
-cols = ['id', 'female', 'year', 'age', 'hsat', 'handdum', 'handper',
-        'hhninc', 'hhkids', 'educ', 'married', 'haupts', 'reals', 'fachhs',
-        'abitur', 'univ', 'working', 'bluec', 'whitec', 'self', 'beamt',
-        'docvis', 'hospvis', 'public', 'addon']
-
-df = pd.read_csv(fldr + 'data/rwm.data', names=cols, sep='\s+',
-                 index_col=['id', 'year'])
-df['const'] = 1
-# model: income ~ exp(age + educ + female)
-
-dta = df.xs(1988, level='year')
-dta = dta[dta.hhninc > 0]  # 2 houses with zero income.
-sub_cols = ['hhninc', 'const', 'age', 'educ', 'female']
-dta = dta[sub_cols]
-
-dta_gmm = df[['hhninc', 'const', 'age', 'educ', 'female', 'hsat',
-              'married']].xs(1988, level='year')
-
-dta_gmm = dta_gmm[dta_gmm.hhninc > 0]
 
 
 class GMM(object):
@@ -106,3 +86,32 @@ class GMM(object):
         round_two = optimize.fmin(self.mom_gen_exp, x0=round_one,
                                   maxiter=maxiter)
         return round_two
+
+if __name__ == '__main__':
+    fldr = '/Users/tom/Dropbox/Economics/Econometrics_2/Greene_Econometrics/'
+    cols = ['id', 'female', 'year', 'age', 'hsat', 'handdum', 'handper',
+            'hhninc', 'hhkids', 'educ', 'married', 'haupts', 'reals', 'fachhs',
+            'abitur', 'univ', 'working', 'bluec', 'whitec', 'self', 'beamt',
+            'docvis', 'hospvis', 'public', 'addon']
+
+    df = pd.read_csv(fldr + 'data/rwm.data', names=cols, sep='\s+',
+                     index_col=['id', 'year'])
+    df['const'] = 1
+    # model: income ~ exp(age + educ + female)
+
+    dta = df.xs(1988, level='year')
+    dta = dta[dta.hhninc > 0]  # 2 houses with zero income.
+    sub_cols = ['hhninc', 'const', 'age', 'educ', 'female']
+    dta = dta[sub_cols]
+
+    dta_gmm = df[['hhninc', 'const', 'age', 'educ', 'female', 'hsat',
+                  'married']].xs(1988, level='year')
+
+    dta_gmm = dta_gmm[dta_gmm.hhninc > 0]
+
+    endog = ['hhninc']
+    exog = ['const', 'age', 'educ', 'female']
+    instruments = ['hsat', 'married']
+
+    obj = GMM(dta_gmm, endog, exog, instruments)
+    res = obj.fit_exp([-1, 1, .1, .2])
