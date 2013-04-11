9e57e668871fe5b37242047d2b8a347f4dea6ae6
TomAugspurger <thomas-augspurger@uiowa.edu>
2013-04-11 08:10:18 -0500

    Using minimize instead of fmin for more solving methods.

--- ex_13_7_class.py
+++ ex_13_7_class.py
@@ -33,12 +33,19 @@ class GMM(object):
         with patsy.
         """
         self.data = data
-        if set.intersection(set(exog), set(instruments)) != set([]):
-            raise ValueError('Don\'t put cols in exog and instruments.')
+        # if set.intersection(set(exog), set(instruments)) != set([]):
+        #     raise ValueError('Don\'t put cols in exog and instruments.')
         self.endog = endog
         self.exog = list(exog)
-        self.instruments = list(instruments)
-        self.n_moms = len(self.exog) + len(self.instruments)
+        try:
+            self.instruments = list(instruments)
+            self.generalized = True
+            self.n_moms = len(self.exog) + len(self.instruments)
+        except TypeError:
+            self.instruments = None
+            self.generalized = False
+            self.n_moms = len(self.exog)
+
         self.n = len(data[endog])
         self.form = form
         self.method = method
@@ -51,14 +58,20 @@ class GMM(object):
         theta = np.array(theta).reshape(len(self.exog), 1)
         y = data[self.endog].values
         X = data[self.exog].values
-        Z = data[self.exog + self.instruments].values
         e = y - np.exp(dot(X, theta))
-        m = dot(Z.T, e) / self.n
-        # pdb.set_trace()
-        try:
-            moments = dot(dot(m.T, self.W), m)
-        except (TypeError, AttributeError):
+
+        if self.generalized:
+            Z = data[self.exog + self.instruments].values
+            m = dot(Z.T, e) / self.n
+            # pdb.set_trace()
+            try:
+                moments = dot(dot(m.T, self.W), m)
+            except (TypeError, AttributeError):
+                moments = dot(m.T, m)
+        else:
+            m = dot(X.T, e) / self.n
             moments = dot(m.T, m)
+        pdb.set_trace()
         return moments
 
     def fit_exp(self, x0, maxiter=None):
@@ -76,20 +89,24 @@ class GMM(object):
         y = self.data[self.endog].values
         X = self.data[self.exog].values
         e = y - np.exp(dot(X, round_one))
-        Z = self.data[self.exog + self.instruments].values
 
-        for i, obs in enumerate(Z):
-            zi = obs.reshape(Z.shape[1], -1)
-            if i == 0:
-                W = dot(zi, zi.T) * (e[i]) ** 2
-            else:
-                W += dot(zi, zi.T) * (e[i]) ** 2
-        self.W = inv(W / self.n)
-        # round_two implicitly uses W.  Probably a better way.
-        # This also caches W forever until explicity removed.
-        round_two = optimize.fmin(self.mom_gen_exp, x0=round_one,
-                                  maxiter=maxiter)
-        return round_two
+        if self.generalized:
+            Z = self.data[self.exog + self.instruments].values
+
+            for i, obs in enumerate(Z):
+                zi = obs.reshape(Z.shape[1], -1)
+                if i == 0:
+                    W = dot(zi, zi.T) * (e[i]) ** 2
+                else:
+                    W += dot(zi, zi.T) * (e[i]) ** 2
+            self.W = inv(W / self.n)
+            # round_two implicitly uses W.  Probably a better way.
+            # This also caches W forever until explicity removed.
+            round_two = optimize.fmin(self.mom_gen_exp, x0=round_one,
+                                      maxiter=maxiter)
+            return round_two
+        else:
+            return round_one
 
 if __name__ == '__main__':
     fldr = '/Users/tom/Dropbox/Economics/Econometrics_2/Greene_Econometrics/'
@@ -103,10 +120,25 @@ if __name__ == '__main__':
     df['const'] = 1
     # model: income ~ exp(age + educ + female)
 
+    #################################################
+    #            Part One: Method of Moments.       #
+    #            Basically just an orthogonality    #
+    #            condition for the regressors.      #
+    #################################################
     dta = df.xs(1988, level='year')
     dta = dta[dta.hhninc > 0]  # 2 houses with zero income.
     sub_cols = ['hhninc', 'const', 'age', 'educ', 'female']
     dta = dta[sub_cols]
+
+    obj = GMM(dta, ['hhninc'], ['const', 'age', 'educ', 'female'], None, form='exp')
+    res = obj.fit_exp([-1.69331, .00207, .04792, -.00658], maxiter=2000)
+    print(res)
+
+    #####################################################
+    #        Part Two: Generalize Method of Moments.    #
+    #        Additional orthogonality requirements      #
+    #        for hsat and married                       #
+    #####################################################
 
     dta_gmm = df[['hhninc', 'const', 'age', 'educ', 'female', 'hsat',
                   'married']].xs(1988, level='year')
